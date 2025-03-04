d3563e76021aad3b65a4ef7f1f2b715b27fcf029
Zachary Hamm <zjhamm304@gmail.com>
2025-03-04 15:52:54 -0500

    add:innit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const FILE_PATHS_DELIMITER = ",";
+
+export const SPEC_MAP_PATH = ".cypress_load_balancer/spec-map.json";
+export const TEMPORARY_SPEC_MAP = ".cypress_load_balancer/spec-map.json";
