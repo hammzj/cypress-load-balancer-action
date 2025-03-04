f5d57afd5ad87630789e47fa2bde94fa758a9d43
Zachary Hamm <zjhamm304@gmail.com>
2025-03-04 16:16:17 -0500

    add:dist files lol

---
+++ dist/src/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TEMPORARY_SPEC_MAP = exports.SPEC_MAP_PATH = exports.FILE_PATHS_DELIMITER = void 0;
+exports.FILE_PATHS_DELIMITER = ",";
+exports.SPEC_MAP_PATH = ".cypress_load_balancer/spec-map.json";
+exports.TEMPORARY_SPEC_MAP = ".cypress_load_balancer/spec-map.json";
