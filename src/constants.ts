203fa7ee5598545fb379641dfd247e9f968521fe
Zachary Hamm <zjhamm304@gmail.com>
2025-03-05 11:38:42 -0500

    add:trying something new

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const FILE_PATHS_DELIMITER = ",";
-
 export const SPEC_MAP_PATH = ".cypress_load_balancer/spec-map.json";
-export const TEMPORARY_SPEC_MAP = ".cypress_load_balancer/spec-map.json";
+export const TEMPORARY_SPEC_MAP_PATH = "./temp/.cypress_load_balancer/spec-map.json";
+export const TEMPORARY_SPEC_MAP_GLOB = "./temp/.cypress_load_balancer/**/spec-map.json";
