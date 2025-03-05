b0b6f60e2f8d54a5a595df2b505824b6079173a2
Zachary Hamm <zjhamm304@gmail.com>
2025-03-05 11:38:57 -0500

    add:trying something new

--- dist/src/constants.js
+++ dist/src/constants.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEMPORARY_SPEC_MAP = exports.SPEC_MAP_PATH = exports.FILE_PATHS_DELIMITER = void 0;
+exports.TEMPORARY_SPEC_MAP_GLOB = exports.TEMPORARY_SPEC_MAP_PATH = exports.SPEC_MAP_PATH = exports.FILE_PATHS_DELIMITER = void 0;
 exports.FILE_PATHS_DELIMITER = ",";
 exports.SPEC_MAP_PATH = ".cypress_load_balancer/spec-map.json";
-exports.TEMPORARY_SPEC_MAP = ".cypress_load_balancer/spec-map.json";
+exports.TEMPORARY_SPEC_MAP_PATH = "./temp/.cypress_load_balancer/spec-map.json";
+exports.TEMPORARY_SPEC_MAP_GLOB = "./temp/.cypress_load_balancer/**/spec-map.json";
