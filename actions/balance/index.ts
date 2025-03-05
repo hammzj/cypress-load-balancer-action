e20aa657ff7f907c0510a3b7cb0f45393128fa50
Zachary Hamm <zjhamm304@gmail.com>
2025-03-05 15:41:25 -0500

    try another way

--- actions/balance/index.ts
+++ actions/balance/index.ts
@@ -1,16 +1,16 @@
-const core = require("@actions/core");
-const cache = require("@actions/cache");
-const { cli } = require("cypress-load-balancer");
-const { getInputAsArray, getInputAsInt } = require("../../src/utils/input");
-const { SPEC_MAP_PATH } = require("../../src/constants");
-
+import core from "@actions/core";
+import cache from "@actions/cache";
+import { cli } from "cypress-load-balancer";
+import { SPEC_MAP_PATH } from "../../src/constants";
+import { getInputAsArray, getInputAsInt } from "../../src/utils/input";
+import {getSpecs} from 'find-cypress-specs'
 async function restoreCachedLoadBalancingMap() {
   try {
     const cachePrimaryKey = core.getInput("cache-primary-key");
     const cacheRestoreKeys = getInputAsArray("cache-restore-keys");
     await cache.restoreCache([SPEC_MAP_PATH], cachePrimaryKey, cacheRestoreKeys);
     //@ts-expect-error Ignore
-  } catch (error) {
+  } catch (error: Error) {
     core.error(error);
   }
 }
@@ -22,13 +22,13 @@ function getArgv() {
   const coreFilePaths = getInputAsArray("file-paths", { trimWhitespace: true });
   const globs = getInputAsArray("glob");
 
-  const filePaths = [];
-  filePaths.push(...coreFilePaths.map((fileName) => [`--files`, fileName]).flat());
+  const filePaths: string[] = [];
+  filePaths.push(...coreFilePaths.map((fileName: string) => [`--files`, fileName]).flat());
 
-  const argv = [`--runners`, runners.toString(), "--testing-type", testingType, `--gha`];
+  const argv = [`--runners`, (runners as number).toString(), "--testing-type", testingType, `--gha`];
   if (format) argv.push(`--format`, format);
   argv.push(...filePaths);
-  argv.push(...globs.map((g) => [`--glob`, g]).flat());
+  argv.push(...globs.map((g: string) => [`--glob`, g]).flat());
 
   return argv;
 }
@@ -37,9 +37,13 @@ async function main() {
   try {
     await restoreCachedLoadBalancingMap();
     const argv = getArgv();
+
+    //THIS SHOULD FAIL
+    console.log(getSpecs(undefined, 'e2e'))
+
     cli.parseSync(argv);
     //@ts-expect-error Ignore
-  } catch (error) {
+  } catch (error: Error) {
     core.setFailed(error.stack);
   }
 }
