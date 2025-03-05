5b2e3cbe2aa2b042a29e90a3ba2f8a3bb2f35c22
Zachary Hamm <zjhamm304@gmail.com>
2025-03-05 15:34:17 -0500

    try another way

--- actions/balance/index.js
+++ actions/balance/index.js
@@ -1,9 +1,8 @@
-import * as core from "@actions/core";
-import * as cache from "@actions/cache";
-import { cli } from "cypress-load-balancer";
-import { getInputAsArray, getInputAsInt } from "../../src/utils/input";
-import { SPEC_MAP_PATH } from "../../src/constants";
-import path from "path";
+const core = require("@actions/core");
+const cache = require("@actions/cache");
+const { cli } = require("cypress-load-balancer");
+const { getInputAsArray, getInputAsInt } = require("../../src/utils/input");
+const { SPEC_MAP_PATH } = require("../../src/constants");
 
 async function restoreCachedLoadBalancingMap() {
   try {
@@ -11,22 +10,22 @@ async function restoreCachedLoadBalancingMap() {
     const cacheRestoreKeys = getInputAsArray("cache-restore-keys");
     await cache.restoreCache([SPEC_MAP_PATH], cachePrimaryKey, cacheRestoreKeys);
     //@ts-expect-error Ignore
-  } catch (error: Error) {
+  } catch (error) {
     core.error(error);
   }
 }
 
-function getArgv(): string[] {
+function getArgv() {
   const runners = getInputAsInt("runners", { required: true });
   const testingType = core.getInput("testing-type", { required: true });
   const format = core.getInput("format");
-  const coreFilePaths: string[] = getInputAsArray("file-paths", { trimWhitespace: true });
-  const globs: string[] = getInputAsArray("glob");
+  const coreFilePaths = getInputAsArray("file-paths", { trimWhitespace: true });
+  const globs = getInputAsArray("glob");
 
-  const filePaths: string[] = [];
-  filePaths.push(...coreFilePaths.map((fileName: string) => [`--files`, fileName]).flat());
+  const filePaths = [];
+  filePaths.push(...coreFilePaths.map((fileName) => [`--files`, fileName]).flat());
 
-  const argv = [`--runners`, (runners as number).toString(), "--testing-type", testingType, `--gha`];
+  const argv = [`--runners`, runners.toString(), "--testing-type", testingType, `--gha`];
   if (format) argv.push(`--format`, format);
   argv.push(...filePaths);
   argv.push(...globs.map((g) => [`--glob`, g]).flat());
@@ -40,7 +39,7 @@ async function main() {
     const argv = getArgv();
     cli.parseSync(argv);
     //@ts-expect-error Ignore
-  } catch (error: Error) {
+  } catch (error) {
     core.setFailed(error.stack);
   }
 }
